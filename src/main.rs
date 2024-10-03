badb238a9b0b55ebc74b9143184647c26ed697a4
Tyrone Hills <mobw4u@gmail.com>
2024-10-03 12:37:06 +0300

    Basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::fmt;
+
+enum Gem {
+    Diamond,
+    Sapphire,
+    Ruby,
+    Topaz,
+    Onyx,
+    Jade,
+}
+
+impl fmt::Display for Gem {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Gem::Diamond => write!(f, "diamond"),
+            Gem::Sapphire => write!(f, "sapphire"),
+            Gem::Ruby => write!(f, "ruby"),
+            Gem::Topaz => write!(f, "topaz"),
+            Gem::Topaz => write!(f, "topaz"),
+            Gem::Topaz => write!(f, "topaz"),
+            Gem::Onyx => write!(f, "onyx"),
+            Gem::Jade => write!(f, "jade"),
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let gems = [
+        (Gem::Onyx, 25.00),
+        (Gem::Diamond, 100.00),
+        (Gem::Onyx, 50.00),
+        (Gem::Ruby, 10.00),
+    ];
+
+    for gem in gems {
+        println!("This {} is worth {}", gem.0, gem.1);
+    }
 }
